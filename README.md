b9bd7290dfcd7b1d3902dbb2de03d610ce020c13
Merzer-git <alanizbriansebastian@outlook.com>
2026-01-14 13:33:30 -0300

    initial_proofs
    Pruebas iniciales de la app definitiva para el análisis estadístico de la Database de SIPRI.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Military Expenditure
- 
+-------
+
+Military Expenditure es una aplicacion que, con datos del SIPRI y del Banco Mundial analiza las principales variables en los presupuestos de defensa de mas de 170 paises desde 1949 - 2024
---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title= "Military Expenditure",
+    page_icon= "🌎"
+)
+
+st.write("Welcome")
+
+st.sidebar.success("Selecciona una pagina")
+
+st.markdown(
+    """
+    ### Pruebas Iniciales de GUI para al app
+    """
+)
